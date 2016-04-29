6abc0332c6b6317054153c94c575a8582bf9a86a
Strick4888 <benjaminstrick2016@u.northwestern.edu>
2016-04-28 22:43:27 -0500

    css changes, interface adjustments, subtle optimizations

--- composite/app/controllers/home_controller.rb
+++ composite/app/controllers/home_controller.rb
@@ -16,18 +16,12 @@ class HomeController < ApplicationController
     # Instagram API Calls
     @client = Instagram.client(:access_token => Instagram.access_token)
     #@instagram = @client.media_search("37.7808851", "-122.3948632")
-
-    puts params[:lat].blank?
-    puts params[:lat]
-    puts params[:lng].blank?
-    puts params[:lng]
-
-    puts params[:current]
     
     unless params[:lat].blank? && params[:long].blank?
-      @instagram = @client.media_search(params[:lat], params[:lng])
+      @instagram = @client.media_search(params[:lat], params[:lng], options = {:count => 50})
     else
-      @instagram = @client.media_search("37.7808851", "-122.3948632")
+      @instagram = @client.media_search("37.7808851", "-122.3948632", options = {:count => 50})
+      params[:current] = "Evanston, IL, USA"
     end
 
     # Alternate way to print content to screen
@@ -41,36 +35,42 @@ class HomeController < ApplicationController
     detector = OpenCV::CvHaarClassifierCascade::load(data)
 
     @img_paths = []
+    num = 0
 
-    # Test of OpenCV with our Instagram images
-    for media2 in @instagram
-      
-      insta_img = Net::HTTP.get(URI.parse(media2.images.standard_resolution.url))
+    for photo in @instagram
+
+      puts num
+      num+=1
+
+      insta_img = Net::HTTP.get(URI.parse(photo.images.standard_resolution.url))
       temp = Tempfile.new(['img','.jpg'])
       insta_img.force_encoding('UTF-8')
       temp.write(insta_img)
+      
+      #debugging step
       puts temp.path
 
+      #add error handling HERE
       image = OpenCV::IplImage.load(temp.path, iscolor = CV_LOAD_IMAGE_GRAYSCALE)
       face_id = 0
 
       detector.detect_objects(image) do |region|
         #image.rectangle! region.top_left, region.bottom_right, :color => color
         face_img = image.sub_rect( region )
-        file_name = media2.id.to_s + "_face_" + face_id.to_s + ".jpg"
+        file_name = photo.id.to_s + "_face_" + face_id.to_s + ".jpg"
         path = "app/assets/images/" + file_name
         face_img.save_image(path)
         face_id += 1
         face = OpenStruct.new
         face.name = file_name
-        face.link = media2.images.standard_resolution.url.to_s
+        face.link = photo.images.standard_resolution.url.to_s
         @img_paths.push(face)
+        break if @img_paths.length >= 21
       end
-
+      break if @img_paths.length >= 21
     end
-    
-  end
 
+  end
 end
 
 
